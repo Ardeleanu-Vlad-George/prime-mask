04e6a0b6474a67c1cedb08c41aa79cafe03d05aa
Ardeleanu Vlad-George <ardeleanu_vlad@ymail.com>
2025-01-22 17:41:48 +0200

    Technically achieved original goal, updated the goal to be more inlign with proper mathematics

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ Generate an image in which pixels are black by default. They are assigned a dark
  is a prime number.
 There is only one CLA expected, the size of the square image. The name of the file is the same as its size.
 The extension is the same for all files, it's '.png'. They are all stored inside './data/'.
+
+What you actually wanted for this project is implementing a nation called the [Ulam spiral](https://en.wikipedia.org/wiki/Ulam_spiral)
--- main.cpp
+++ main.cpp
@@ -8,6 +8,13 @@ void log_help(std::ostream& os){
   os<<"Provide the size of the square image to be generated\n";
 }
 
+int is_prime(int nr){
+  for(int i=2; i < nr/2; i++)
+    if(nr % i == 0)
+      return 0;
+  return 1;
+}
+
 int main(int argc, char *argv[]){
   if(2 == argc && 0 == strcmp(argv[1], "help")){
     log_help(std::cout);
@@ -19,6 +26,14 @@ int main(int argc, char *argv[]){
     return -1;
   }
   //Program waits for first argument to be a number, but doesn't check it
-  std::cout<<"Normal behaviour\n";
+  int sz = atoi(argv[1]);
+  sf::Image img;
+  img.create(sz, sz);
+  for(int i=0; i < sz*sz; i++)
+    if(is_prime(i))
+      img.setPixel(i%sz, i/sz, sf::Color(150, 150, 150));
+  char file[311];
+  sprintf(file, "data/%d.png", sz);
+  img.saveToFile(file);
   return 0;
 }
