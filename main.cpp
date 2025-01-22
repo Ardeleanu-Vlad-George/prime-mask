b3c0beec647bd79a5ef2689dd5b67bc8e665fa0c
Ardeleanu Vlad-George <ardeleanu_vlad@ymail.com>
2025-01-22 20:33:55 +0200

    All changes but the logic to compute the ulma spiral index

--- main.cpp
+++ main.cpp
@@ -5,7 +5,13 @@
 #include <SFML/Graphics.hpp>
 
 void log_help(std::ostream& os){
-  os<<"Provide the size of the square image to be generated\n";
+  os<<"Provide an odd number as the size of the square image to be generated\n";
+}
+
+//temporary part, just to check the function works properly
+//the faulty logic will then be replaced
+int ulam_index(int x, int y, int s){
+  return x+y*s;
 }
 
 int is_prime(int nr){
@@ -27,11 +33,17 @@ int main(int argc, char *argv[]){
   }
   //Program waits for first argument to be a number, but doesn't check it
   int sz = atoi(argv[1]);
+  if(sz % 2 == 0){
+    std::cerr<<"Wrong parity\n";
+    log_help(std::cerr);
+    return -2;
+  }
   sf::Image img;
-  img.create(sz, sz);
-  for(int i=0; i < sz*sz; i++)
-    if(is_prime(i))
-      img.setPixel(i%sz, i/sz, sf::Color(150, 150, 150));
+  img.create(sz, sz, sf::Color::White);
+  for(int x=0; x < sz; x++)
+    for(int y=0; y < sz; y++)
+      if(is_prime(ulam_index(x, y, sz)))
+        img.setPixel(x, y, sf::Color::Black);
   char file[311];
   sprintf(file, "data/%d.png", sz);
   img.saveToFile(file);
